7400e0577feba3de9795bd2cc38d88b9fc8e095a
AlexZeb80 <60985129+AlexZeb80@users.noreply.github.com>
2022-08-30 09:38:24 -0500

    added console log for test ep

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-  res.status(200).send("Endpoint response text")
+  res.status(200).send("Endpoint response text");
+  console.log("\test endpoint completed");
 })
 module.exports = app;
